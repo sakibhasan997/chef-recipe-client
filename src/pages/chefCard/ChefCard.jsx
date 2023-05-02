835256be8bfac6278146a9186fe725ec0523c36a
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-05-02 22:36:30 +0600

    add rating

--- src/pages/chefCard/ChefCard.jsx
+++ src/pages/chefCard/ChefCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaThumbsUp } from 'react-icons/fa';
+import { FaRegStar, FaStar, FaThumbsUp } from 'react-icons/fa';
+import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 
 const ChefCard = ({ recipe }) => {
@@ -8,15 +9,24 @@ const ChefCard = ({ recipe }) => {
         <div className='w-full '>
             <div>
                 <div className="card w-full bg-base-100 shadow-xl">
-                <figure> <img className='h-64 w-full object-cover' src={picture} alt="Shoes" /></figure>
+                    <figure> <img className='h-64 w-full object-cover' src={picture} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title text-xl font-semibold">Name: {name}</h2>
                         <hr />
                         <p>Years Of Experience: {yearsOfExperience}</p>
                         <p> Recipes: {numberOfRecipes}</p>
-                        <p className='flex gap-2'> <FaThumbsUp/> {likes}</p>
-                        <div className="card-actions justify-end">
-                        <Link to={`/recipes/${id}`}><button className="btn btn-warning text-white">View Recipes</button></Link> 
+                        <p className='flex gap-2'> <FaThumbsUp /> {likes}</p>
+                        <div className="card-actions justify-between items-center">
+                            <div>
+                                <Rating
+                                    placeholderRating={rating}
+                                    emptySymbol={<FaRegStar />}
+                                    placeholderSymbol={<FaStar className='text-warning' />}
+                                    fullSymbol={<FaStar className='text-warning' />}
+                                />
+                                <span>{rating}</span>
+                            </div>
+                            <Link to={`/recipes/${id}`}><button className="btn btn-warning text-white">View Recipes</button></Link>
                         </div>
                     </div>
                 </div>
