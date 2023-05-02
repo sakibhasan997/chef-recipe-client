69c5d049e27bd6ec4ed06ca4dd7b742316520f0e
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-05-02 12:09:37 +0600

    complete the card

--- src/pages/chefCard/ChefCard.jsx
+++ src/pages/chefCard/ChefCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaThumbsUp } from 'react-icons/fa';
 
 const ChefCard = ({ recipe }) => {
     const { id, name, picture, category, yearsOfExperience, bio, numberOfRecipes, likes, description, numberOfRecipesId, rating } = recipe;
@@ -8,10 +9,13 @@ const ChefCard = ({ recipe }) => {
                 <div className="card w-full bg-base-100 shadow-xl">
                 <figure> <img className='h-64 w-full object-cover' src={picture} alt="Shoes" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Name: {name}</h2>
+                        <h2 className="card-title text-xl font-semibold">Name: {name}</h2>
+                        <hr />
                         <p>Years Of Experience: {yearsOfExperience}</p>
+                        <p> Recipes: {numberOfRecipes}</p>
+                        <p className='flex gap-2'> <FaThumbsUp/> {likes}</p>
                         <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+                            <button className="btn btn-warning text-white">View Recipes</button>
                         </div>
                     </div>
                 </div>
